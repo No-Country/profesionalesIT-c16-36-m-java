eae9449dd305f5b761ccb5298b0aa2972d5cc7e1
“Claudia <claudia.ortiz@uc.cl>
2024-02-26 14:07:18 -0300

    feat([01-11]):add Language Entity

---
+++ professionalIT_back/src/main/java/com/nocountry/professionalIT/entities/LanguageEntity.java
@@ -0,0 +1,32 @@
+package com.nocountry.professionalIT.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Entity class representing a language.
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "languages")
+public class LanguageEntity {
+
+
+    /**
+     * Unique identifier for a language.
+     */
+    @Id
+    @Column(name= "lang_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+    /**
+     * Name of the language.
+     */
+    @Column(name = "lang_name")
+    private String name;
+}
