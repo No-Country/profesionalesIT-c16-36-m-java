b95b8b6fa6593791b85bf26782e894ecf2d2098e
“Claudia <claudia.ortiz@uc.cl>
2024-02-26 16:35:30 -0300

    feat([01-19]):add HardSkill Entity

---
+++ professionalIT_back/src/main/java/com/nocountry/professionalIT/entities/HardSkillEntity.java
@@ -0,0 +1,33 @@
+package com.nocountry.professionalIT.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Entity class representing a hard skill.
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "hard_skills")
+public class HardSkillEntity {
+
+    /**
+     * Unique identifier for the hard skill.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hs_id")
+    private Integer id;
+
+    /**
+     * Name of the hard skill.
+     */
+    @Column(name = "hs_name")
+    private String name;
+}
