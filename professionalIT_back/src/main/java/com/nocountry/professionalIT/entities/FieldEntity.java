5620bf981ee278a40be81059afd5394f51f05d4f
“Claudia <claudia.ortiz@uc.cl>
2024-02-26 16:34:34 -0300

    feat([01-17]):add Field Entity

---
+++ professionalIT_back/src/main/java/com/nocountry/professionalIT/entities/FieldEntity.java
@@ -0,0 +1,31 @@
+package com.nocountry.professionalIT.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * Entity class representing a field in the IT industry.
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name = "fields")
+public class FieldEntity {
+
+    /**
+     * Unique identifier for the field.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "field_id")
+    private Integer id;
+
+    /**
+     * Name of the field.
+     */
+    @Column(name = "field_name")
+    private String name;
+
+}
