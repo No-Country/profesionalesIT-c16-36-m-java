3da5a1711479185472187acfd0f4ada4745acdc1
“Claudia <claudia.ortiz@uc.cl>
2024-02-27 14:31:37 -0300

    Add([02]5-9): Add Documentation to Repositories

--- professionalIT_back/src/main/java/com/nocountry/professionalIT/repository/HardSkillRepository.java
+++ professionalIT_back/src/main/java/com/nocountry/professionalIT/repository/HardSkillRepository.java
@@ -1,7 +1,15 @@
 package com.nocountry.professionalIT.repository;
 
+import com.nocountry.professionalIT.entities.CompanyEntity;
 import com.nocountry.professionalIT.entities.HardSkillEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Repository interface for managing operations on the {@link HardSkillEntity} entity.
+ * Extends {@link JpaRepository} to inherit basic CRUD (Create, Read, Update, Delete) operations.
+ *
+ * @author Claudia Ortiz
+ * @version 1.0
+ * @since 2024-02-26
+ */
 public interface HardSkillRepository extends JpaRepository<HardSkillEntity, Integer> {
 }
--- professionalIT_back/src/main/java/com/nocountry/professionalIT/repository/SoftSkillRepository.java
+++ professionalIT_back/src/main/java/com/nocountry/professionalIT/repository/SoftSkillRepository.java
@@ -1,7 +1,15 @@
 package com.nocountry.professionalIT.repository;
 
+import com.nocountry.professionalIT.entities.CompanyEntity;
 import com.nocountry.professionalIT.entities.SoftSkillEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+/**
+ * Repository interface for managing operations on the {@link SoftSkillEntity} entity.
+ * Extends {@link JpaRepository} to inherit basic CRUD (Create, Read, Update, Delete) operations.
+ *
+ * @author Claudia Ortiz
+ * @version 1.0
+ * @since 2024-02-26
+ */
 public interface SoftSkillRepository extends JpaRepository<SoftSkillEntity, Integer> {
 }
