8a9a9fd335512188b2e9b7fdd9bf14bc318fcde3
karen <quemojojojo@gmail.com>
2024-02-29 09:32:33 -0500

    config: materialtailwind dependencies for theme

--- professionalIT_front/ITProfessionals/src/main.jsx
+++ professionalIT_front/ITProfessionals/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { ThemeProvider } from '@material-tailwind/react';
 import Root from './Router.jsx';
-import Landing from './pages/public/landing/Landing';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
