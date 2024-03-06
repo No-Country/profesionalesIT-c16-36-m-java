ab579acb61cd530faf538133b6856f18aceade1e
karen <quemojojojo@gmail.com>
2024-03-06 15:55:11 -0500

    config: api axios methods

---
+++ professionalIT_front/ITProfessionals/src/api/index.jsx
@@ -0,0 +1,72 @@
+import axios from 'axios'
+
+const url = "https://jobsearch-350323.ue.r.appspot.com/"
+
+const instance = axios.create({
+    baseURL:url
+})
+
+const post = (url,data)=>{
+    return instance.post(url,data)
+}
+
+const get = (url)=>{
+    return instance.get(url)
+}
+
+const getWithToken = async (url)=>{
+    const token = await localStorage.getItem('token')
+    if(token){
+        return await instance.get(url,{
+            headers:{
+                'Authorization':"Bearer "+token
+            }
+        })
+    }
+}
+
+
+const postWithToken = async (url,data)=>{
+    const token = localStorage.getItem("token")
+    if(token){
+        return await instance.post(url,data,{
+            headers:{
+                'Authorization':"Bearer "+localStorage.getItem("token")
+            }
+        })
+    }
+
+    return {
+        data:{
+            failed:true,
+            message:"No tienes token"
+        }
+    }
+    
+}
+
+const putWithToken = async (url,data)=>{
+    const token = localStorage.getItem("token")
+    if(token){
+        return await instance.put(url,data,{
+            headers:{
+                'Authorization':"Bearer "+localStorage.getItem("token")
+            }
+        })
+    }
+}
+
+const deleteWithToken = async (url)=>{
+    const token = localStorage.getItem("token")
+    if(token){
+        return await instance.delete(url,{
+            headers:{
+                'Authorization':"Bearer "+localStorage.getItem("token")
+            }
+        })
+    }
+}
+
+export default instance
+
+export {post,postWithToken,get,getWithToken,putWithToken,deleteWithToken}
