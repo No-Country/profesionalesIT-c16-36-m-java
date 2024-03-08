13bdc8d627bdf763a0b6a4f3205b7133ff013a41
clauo <claudia.ortiz@uc.cl>
2024-03-08 13:28:15 -0300

    Add: Availability and Company Controllers

--- professionalIT_back/src/main/java/com/nocountry/professionalIT/controller/AvailabilityController.java
+++ professionalIT_back/src/main/java/com/nocountry/professionalIT/controller/AvailabilityController.java
@@ -1,12 +1,26 @@
 package com.nocountry.professionalIT.controller;
 
+
+import com.nocountry.professionalIT.dto.AvailabilityDto;
+import com.nocountry.professionalIT.service.AvailabilityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("availability")
 public class AvailabilityController {
-	
-	//service needed
+
+    private final AvailabilityService service;
+    @GetMapping
+    public ResponseEntity<List<AvailabilityDto>> findAllAvailabilities (){
+        return ResponseEntity.ok().body(service.availabilityList());
+    }
 
 }
